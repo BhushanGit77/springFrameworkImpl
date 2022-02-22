2c9738bc8d011a46b43cea068446dff1c0993c84
BhushanGit77 <bhushanjain77@gmail.com>
2022-02-22 18:24:23 +0530

    Spring mavenProject creation

---
+++ src/main/java/com/bhushan/DemoSpringFramework/App.java
@@ -0,0 +1,13 @@
+package com.bhushan.DemoSpringFramework;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
